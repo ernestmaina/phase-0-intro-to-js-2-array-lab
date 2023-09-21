bf99b3b47b7378530d3c15b3f1559c82f6b57b2c
ErnestMaina <you@example.com>
2023-09-21 17:36:52 +0300

    unable to complete all the tests

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function  destructivelyAppendCat(name){
+    cats.push(name);
+    return cats.splice();
+}
+
+
+function  destructivelyPrependCat(name){
+    cats.unshift(name);
+    return cats.splice();
+}
+
+
+function  destructivelyRemoveLastCat(name){
+    cats.pop(name);
+    return cats.splice();
+}
+
+function  destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+   
+}
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
